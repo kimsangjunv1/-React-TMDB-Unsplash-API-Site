f83f0cba7c09bdf15c2a6c601be4d47ef46da8ee
kimsangjunv1 <ingyoe64@gmail.com>
2022-11-07 19:01:05 +0900

    “2022.11.07”

--- src/components/include/MovieCont.jsx
+++ src/components/include/MovieCont.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 
-const MovieCont = ({ movies }) => {
-  //   console.log(movies);
-  return <div>{movies}</div>;
-};
+function MovieItem(props) {
+  return (
+    <li>
+      <a href={`https://www.themoviedb.org/movie/${props.movie.id}`}>
+        {/* <img src="" alt="" /> */}
+        <img
+          src={`https://image.tmdb.org/t/p/w500${props.movie.poster_path}`}
+          alt={props.movie.original_title}
+        />
+        <span className="title">{props.movie.original_title}</span>
+        <span className="star">{props.movie.vote_average}</span>
+      </a>
+    </li>
+  );
+}
+
+function MovieCont(props) {
+  // console.log(movies);
+  return (
+    <section className="cont__movie">
+      <div className="container">
+        <div className="movie__inner">
+          <ul>
+            {props.movies.map((movies, index) => (
+              <MovieItem key={index} movie={movies} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
 
 export default MovieCont;
