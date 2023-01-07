7742c055c2d6cd3b58b5b2e53f9c3d1f8168f243
kimsangjunv1 <ingyoe64@gmail.com>
2023-01-08 01:38:10 +0900

    2023.01.08

--- src/components/include/YoutubeSlider.jsx
+++ src/components/include/YoutubeSlider.jsx
@@ -6,10 +6,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-coverflow";
-// import "swiper/css/pagination";
-
-// import "./styles.css";
-
 
 //데이터 적기
 
